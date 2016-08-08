c755af3e0d9e509d25e437bd100135276e29a955
Brian Steere <dianoga7@3dgo.net>
2016-08-08 09:59:54 -0500

    Improve build scripts

--- config/babel.prod.js
+++ config/babel.prod.js
@@ -9,7 +9,7 @@ module.exports = {
     'babel-plugin-syntax-trailing-function-commas',
     'babel-plugin-transform-class-properties',
     'babel-plugin-transform-object-rest-spread',
-    'babel-plugin-transform-react-constant-elements',
+    // 'babel-plugin-transform-react-constant-elements',
   ].map(require.resolve).concat([
     [require.resolve('babel-plugin-transform-runtime'), {
       helpers: false,
